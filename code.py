f831849061405c66a9cb51f3ed370d808af85f44
Edward Morgan <edwardm72@gmail.com>
2018-02-26 17:01:38 -0500

    Added code & test file

---
+++ code.py
@@ -0,0 +1,8 @@
+def returnArgument(retVal):
+    return retVal
+
+def multby10(mult):
+    return 11 * mult
+
+def finalResult():
+    return returnArgument(10) + multby10(10)
---
+++ test_code.py
@@ -0,0 +1,6 @@
+import code
+import unittest
+
+assert code.returnArgument(90) == 90
+assert code.multby10(7) == 100
+assert code.finalResult() == 110
